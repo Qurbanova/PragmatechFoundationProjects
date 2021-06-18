9115690261331a4f79d9a2350dbc4431927b2ca0
Qurbanova <gunel.qurbanova@aiesec.net>
2021-06-18 00:57:03 -0700

    first

---
+++ Works/js-practice/app.js
@@ -0,0 +1,47 @@
+let count = 5;
+const count1 = 10;
+console.log(count);
+console.log(count1);
+let sum = count + count1;
+console.log(sum);
+let nameNew = 'Elcin';
+let sum2 = nameNew + count;
+console.log(sum2);
+count = 5;
+
+let count2 = count ** 3;
+console.log(count2);
+
+let count3 = 100;
+let modulus = 100 % 7;
+console.log(modulus);
+
+let count4 = 8;
+count4++;
+console.log(count4);
+
+let count5 = 6;
+count5--;
+console.log(count5);
+
+let count6 = 23;
+count6 += 7;
+sum = 7 + 23;
+console.log(count6);
+console.log(sum);
+
+let num = 5;
+console.log(typeof num);
+let str = 'Gunel';
+console.log(typeof str);
+let bool = true;
+console.log(typeof bool);
+console.log(typeof person);
+
+
+let str1 = "Please locate where 'locate' occurs!";
+console.log(str1);
+console.log(str1.length);
+console.log(str1.indexOf('locate'));
+console.log(str1.lastIndexOf('locate'));
+console.log(str1.includes('l'));
